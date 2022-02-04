df9e65ebfe8b9eb0bfa829c4d9adaf6ca761e56d
mint <danielavargashidalgo@gmail.com>
2022-02-03 19:12:42 -0500

    responsive cel tracker-1

--- js/tracker-1.js
+++ js/tracker-1.js
@@ -1,17 +1,24 @@
+let fechaA = document.getElementById('fecha')
+Date.nombreMes = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Abril', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+
+function obtenerNombreCompletoMes(fecha){
+return Date.nombreMes[fecha.getMonth()];
+}
+console.log(obtenerNombreCompletoMes(new Date()));
+
+
+
+
 
 let ejemplo = document.getElementById('ejemplo')
 
 let select = document.getElementById('select')
-const getData = async()=> {
+const getData = async () => {
     const resp = await fetch('https://disease.sh/v3/covid-19/countries')
     const data = await resp.json()
-        data.forEach(element => {
-            const country = select.innerHTML +=  `<option value=${element.country}>${element.country}</option>`
-        console.log(element.cases)
-        })
+    data.forEach(element => {
+        const country = select.innerHTML += `<option value=${element.country}>${element.country}</option>`
+        
+    })
 }
 getData()
-
-
-
-
