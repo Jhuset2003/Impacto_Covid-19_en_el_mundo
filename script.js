b9023a6545b263cfca53a6ee2cb92d58faa830bd
Albert Quintanilla <jhusetqp@gmail.com>
2022-02-07 15:14:06 -0500

    Slider terminado

---
+++ script.js
@@ -0,0 +1,6 @@
+const itemsSlider = document.getElementsByClassName(".items-slider")
+console.log(itemsSlider)
+
+itemsSlider.addEventListener("mousedown" =>{
+    itemsSlider.classList.add("acctive")
+})
